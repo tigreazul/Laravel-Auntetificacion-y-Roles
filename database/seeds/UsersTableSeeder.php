2b59864ac6e9a734a2768e303cd01ac09196240c
Junior <tello_jr2@hotmail.com>
2019-06-19 23:39:18 -0500

    Autentificacion de usuarios por roles

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App/User
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        // $role = new Users();
+        // $role->name = 'admin';
+        // $role->description = 'Administrator';
+        // $role->save();
+
+    }
+}
